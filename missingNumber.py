3be60edfae6ddc43e87140a63b0b97f610a60b8d
Alok Pratap Singh <60225218+AlokPratapSingh22@users.noreply.github.com>
2022-07-28 02:00:34 +0530

    Update missingNumber.py

--- missingNumber.py
+++ missingNumber.py
@@ -10,6 +10,14 @@ def missingNumber_1(nums: List[int]) -> int:
     
     return res
 
+# USING SUM(RANGE) - SUM(ARRAY)
+def missingNumber_3(self, nums: List[int]) -> int:        
+        r = len(nums)
+               
+        for i, n in enumerate(nums): r += (i-n)
+            
+        return r
+
 def missingNumber_2(nums: List[int]) -> int:        
     """Brute solution"""
     for i in range(0, len(nums)+1):
